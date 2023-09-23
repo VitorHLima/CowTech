1570228bf0d1d3e681f7c2bfcedf2d9b71e4fb56
VitorHLima <vitor.oliveira@sptech.school>
2023-09-22 22:34:45 -0300

    Primeiro commit da Sprint2

---
+++ Banco de Dados/Script Banco e Tabelas.sql
@@ -0,0 +1,47 @@
+-- BANCO 
+CREATE DATABASE bdsistema;
+
+USE bdsistema;
+
+-- TABELAS
+
+CREATE TABLE empresa (
+id_empresa INT PRIMARY KEY AUTO_INCREMENT,
+cnpj CHAR(14) UNIQUE NOT NULL,
+nome_empresa VARCHAR(50) NOT NULL,
+representante VARCHAR(40) NOT NULL,
+ddd CHAR(2) NOT NULL,
+telefone CHAR(9) NOT NULL,
+lagradouro VARCHAR(50) NOT NULL,
+bairro VARCHAR(20) NOT NULL,
+municipio VARCHAR(20) NOT NULL,
+estado VARCHAR(20) NOT NULL,
+cep CHAR(8) NOT NULL,
+email VARCHAR(30) NOT NULL,
+senha VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE funcionario (
+id_funcionario INT PRIMARY KEY AUTO_INCREMENT, 
+nome VARCHAR (40) NOT NULL,
+ddd CHAR(2) NOT NULL,
+telefone CHAR(9) NOT NULL,
+email VARCHAR(30) NOT NULL,
+senha CHAR (20) NOT NULL,
+id_empresa INT NOT NULL,
+CONSTRAINT fk_id_empresa FOREIGN KEY (id_empresa)
+REFERENCES empresa (id_empresa)
+);
+
+CREATE TABLE registro (
+id_registro INT PRIMARY KEY AUTO_INCREMENT,
+dtAtual DATETIME DEFAULT CURRENT_TIMESTAMP,
+temperatura FLOAT(4) NOT NULL,
+umidade INT NOT NULL 
+);
+
+CREATE TABLE sensor(
+id_sensor INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(20) NOT NULL,
+codigo VARCHAR(10) NOT NULL
+);
