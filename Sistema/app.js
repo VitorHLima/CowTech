36a6adf78074596a5f893083e97fa8faca2210d1
VitorHLima <vitor.oliveira@sptech.school>
2023-11-14 22:38:42 -0300

    Teste

--- Sistema/app.js
+++ Sistema/app.js
@@ -9,40 +9,34 @@ var PORTA = process.env.AMBIENTE_PROCESSO == "desenvolvimento" ? 3333 : 8080;
 var app = express();
 
 var indexRouter = require("./src/routes/index");
-<<<<<<< HEAD
 var usuarioRouter = require("./src/routes/usuarios");
-var medidasRouter = require("./src/routes/medidas");
+// var medidasRouter = require("./src/routes/medidas");
 var aquariosRouter = require("./src/routes/aquarios");
-var enderecoRouter = require("./src/routes/endereco");
-=======
+var enderecoRouter = require("./src/routes/enderecos");
 // var usuarioRouter = require("./src/routes/usuarios");
 // var medidasRouter = require("./src/routes/medidas");
 // var aquariosRouter = require("./src/routes/aquarios");
-var enderecoRouter = require("./src/routes/enderecos");
->>>>>>> e1e9c3275c40c65fa75809eb2a1142ec3f5b5de0
+// var enderecoRouter = require("./src/routes/enderecos");
 var empresasRouter = require("./src/routes/empresas");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-<<<<<<< HEAD
 app.use(express.static(path.join(__dirname, "Página Inicial")));
 app.use(express.static(path.join(__dirname, "cadastro-empresa")));
 app.use(express.static(path.join(__dirname, "endereco")));
 app.use(express.static(path.join(__dirname, "login")));
 app.use(express.static(path.join(__dirname, "sistemaSensor")));
 app.use(express.static(path.join(__dirname, "img")));
-=======
 app.use(express.static(path.join(__dirname, "public/Site/Página Inicial")));
 app.use(express.static(path.join(__dirname, "public/Site")));
 app.use(express.static(path.join(__dirname, "public")));
->>>>>>> e1e9c3275c40c65fa75809eb2a1142ec3f5b5de0
 
 app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
 app.use("/enderecos", enderecoRouter);
-app.use("/medidas", medidasRouter);
+// app.use("/medidas", medidasRouter);
 app.use("/aquarios", aquariosRouter);
 app.use("/empresas", empresasRouter);
 
---
+++ Sistema/src/controllers/fazendaController.js
@@ -0,0 +1,33 @@
+var fazendaModel = require("../models/fazendaModel");
+
+function publicar(req, res) {
+    var nome = req.body.nomeVar;
+    var endereco = req.body.enderecoVar;
+    var idUsuario = req.body.idUsuario;
+
+    if (nome == undefined) {
+        res.status(400).send("O nome está indefinido!");
+    } else if (endereco == undefined) {
+        res.status(400).send("O endereço está indefinido!");
+    } else if (idUsuario == undefined) {
+        res.status(400).send("O endereço está indefinido!");
+    } else {
+        fazendaModel.publicar(nome, endereco, idUsuario)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            )
+            .catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log("Houve um erro ao realizar o post: ", erro.sqlMessage);
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+}
+
+module.exports = {
+    publicar
+}
