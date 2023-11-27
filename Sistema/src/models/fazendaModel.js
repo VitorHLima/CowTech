4dd0015bc95341f49fa18280ef473278a9121abb
matheusrabellosantos <matheus.csantos@sptech.school>
2023-11-26 23:35:56 -0300

    alterando nome de select

--- Sistema/src/models/fazendaModel.js
+++ Sistema/src/models/fazendaModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config");
 function publicar(nome, idEndereco) {
     console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function publicar(): ");
     var instrucao = `
-        INSERT INTO curral(nomeCurral, FkEndFazenda) VALUES ('${nome}', '${idEndereco}');
+        INSERT INTO curral(nome, FkEndFazenda) VALUES ('${nome}', '${idEndereco}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
